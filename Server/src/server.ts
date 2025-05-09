b82bcc060c9f83e7a47d98711726634be3800323
johnnycls <goulonghuangdi@gmail.com>
2025-05-09 12:05:26 +0800

    try fixing cors

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -22,7 +22,8 @@ const port = PORT || 8080;
 
 app.use(
   cors({
-    origin: [WEB_URL || "http://localhost:5173"],
+    origin: true,
+    // origin: [WEB_URL || "http://localhost:5173"],
   })
 );
 
