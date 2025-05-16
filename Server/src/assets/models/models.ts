d61e50ffe5d6592b5e3bd7579bb51f07c49b9a98
johnnycls <goulonghuangdi@gmail.com>
2025-05-16 20:22:17 +0800

    ai deleted code

--- Server/src/assets/models/models.ts
+++ Server/src/assets/models/models.ts
@@ -6,7 +6,7 @@ import model002 from "./002";
 import model003 from "./003";
 import model004 from "./004";
 import model005 from "./005";
-
+import model014 from "./014";
 export const models: { [key: string]: Model } = {
   "000": model000,
   "001": model001,
@@ -14,6 +14,7 @@ export const models: { [key: string]: Model } = {
   "003": model003,
   "004": model004,
   "005": model005,
+  "014": model014,
 };
 
 export default models;
