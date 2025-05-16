e2bbd8d58ab4a9b87db2b4b73411ef24bf66a78b
johnnycls <goulonghuangdi@gmail.com>
2025-05-16 20:19:24 +0800

    added 6 characters

--- Server/src/assets/models/models.ts
+++ Server/src/assets/models/models.ts
@@ -1,9 +1,19 @@
 import Model from "./model";
 
-import model014 from "./014";
+import model000 from "./000";
+import model001 from "./001";
+import model002 from "./002";
+import model003 from "./003";
+import model004 from "./004";
+import model005 from "./005";
 
 export const models: { [key: string]: Model } = {
-  "014": model014,
+  "000": model000,
+  "001": model001,
+  "002": model002,
+  "003": model003,
+  "004": model004,
+  "005": model005,
 };
 
 export default models;
--- Web/src/assets/models.ts
+++ Web/src/assets/models.ts
@@ -8,6 +8,48 @@ export type Model = {
 
 export const MODELS: Model[] = [
   {
+    _id: "000",
+    name: "葉虎",
+    gender: "male",
+    languages: ["zh-HK"],
+    avatar: "/avatars/000.jpg",
+  },
+  {
+    _id: "001",
+    name: "梁志強老師",
+    gender: "male",
+    languages: ["zh-HK"],
+    avatar: "/avatars/001.jpg",
+  },
+  {
+    _id: "002",
+    name: "林詠詩老師",
+    gender: "female",
+    languages: ["zh-HK"],
+    avatar: "/avatars/002.jpg",
+  },
+  {
+    _id: "003",
+    name: "何美詩",
+    gender: "female",
+    languages: ["zh-HK"],
+    avatar: "/avatars/003.jpg",
+  },
+  {
+    _id: "004",
+    name: "司徒浩然",
+    gender: "male",
+    languages: ["zh-HK"],
+    avatar: "/avatars/004.jpg",
+  },
+  {
+    _id: "005",
+    name: "方力行老師",
+    gender: "male",
+    languages: ["zh-HK"],
+    avatar: "/avatars/005.jpg",
+  },
+  {
     _id: "014",
     name: "米亞 (Mya)",
     gender: "female",
